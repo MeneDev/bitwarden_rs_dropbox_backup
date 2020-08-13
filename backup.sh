4fe2178baf0176f3875da518eca70d666cf0342c
Michael Arndt <michael.arndt@grandcentrix.net>
2020-08-13 11:53:51 +0200

    Make more configurable, add captain-definition

--- backup.sh
+++ backup.sh
@@ -2,15 +2,17 @@
 
 # create backup filename
 BACKUP_FILE="db.sqlite3_$(date "+%F-%H%M%S")"
+SOURCE_FILE="${SOURCE_FILE:-/db.sqlite3}"
 
+TMP_FOLDER="$(mktemp -d)"
 # use sqlite3 to create backup (avoids corruption if db write in progress)
-sqlite3 /db.sqlite3 ".backup '/tmp/db.sqlite3'"
+sqlite3 "${SOURCE_FILE}" ".backup '${TMP_FOLDER}/db.sqlite3'"
 
 # tar up backup and encrypt with openssl and encryption key
-tar -czf - /tmp/db.sqlite3 | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
+tar -czf - "${TMP_FOLDER}/db.sqlite3" | openssl enc -e -aes256 -salt -pbkdf2 -pass "pass:${BACKUP_ENCRYPTION_KEY}" -out "${TMP_FOLDER}/${BACKUP_FILE}.tar.gz"
 
 # upload encrypted tar to dropbox
-/dropbox_uploader.sh upload /tmp/${BACKUP_FILE}.tar.gz /${BACKUP_FILE}.tar.gz
+/dropbox_uploader.sh upload "${TMP_FOLDER}/${BACKUP_FILE}.tar.gz" "/${BACKUP_FILE}.tar.gz"
 
 # cleanup tmp folder
 rm -rf /tmp/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 # setup dropbox uploader config file
 echo "OAUTH_ACCESS_TOKEN=${DROPBOX_ACCESS_TOKEN}" > ~/.dropbox_uploader
 
+# setup cron, default to daily
+CRON_SPEC="${CRON_SPEC:-0 1 * * *}"
+echo "${CRON_SPEC} /backup.sh" > /etc/crontabs/root
+
 # run backup once on container start to ensure it works
 /backup.sh
 
